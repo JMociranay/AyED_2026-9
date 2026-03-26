aebd6c43857be56b845f11842a012e7d8aed19b0
JMociranay <hildaj127@gmail.com>
2026-03-26 16:32:58 -0300

    Add files via upload

---
+++ Yanarico Juan - TP1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juan_Yanarico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string pelicula_favorita;
+
+            Console.WriteLine("hola nuevo usuario");
+            Console.Write("¿Cual es tu nombre?: ");
+            nombre = Console.ReadLine();         
+            Console.Write("¿Cual es tu pelicula favorita?: ");
+            pelicula_favorita = Console.ReadLine();
+            Console.WriteLine("hola "+ nombre +" Tu pelicula preferida "+ pelicula_favorita +" es una porqueria");
+        }
+    }
+}
