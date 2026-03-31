db90593e54b5f728026e91454c64abd9e60c7e20
JMociranay <hildaj127@gmail.com>
2026-03-31 14:15:07 -0300

    Add files via upload

---
+++ Yanarico Juan - TP2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yanarico_Juan___TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string juego_favorito;
+            string nombre;
+            string consola;
+            Console.WriteLine("hola usuario");
+            Console.Write("¿cual es tu nombre?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu juego favorito?: ");
+            juego_favorito = Console.ReadLine();
+            Console.Write("¿en que consola se juega?: ");
+            consola = Console.ReadLine();
+            Console.WriteLine("hola "+  nombre + " tu juego favorito es "+ juego_favorito+ " y se juega en "+ consola);
+
+
+
+
+        }
+    }
+}
