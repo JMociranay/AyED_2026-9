d74d822d8335f9b0909dfc90de3cabd97ee065a7
JMociranay <hildaj127@gmail.com>
2026-04-14 16:45:49 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales/Yanarico Juan - TP2(Condicionales).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            Console.Write("ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("ingrese su edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+            if ( edad >= 18)
+            {
+                Console.WriteLine("Bienvenido al club " + nombre);
+            }
+            else
+            {
+                Console.WriteLine("Lo siento, eres muy polluelo para esto");
+            }
+        }
+    }
+}
