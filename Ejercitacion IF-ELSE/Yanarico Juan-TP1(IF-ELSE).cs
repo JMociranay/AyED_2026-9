c7f9a1d7a8ce2b4d4396670d22c8913e77d863de
JMociranay <hildaj127@gmail.com>
2026-04-14 14:29:15 -0300

    Add files via upload

---
+++ Ejercitacion IF-ELSE/Yanarico Juan-TP1(IF-ELSE).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso_carga;
+            Console.WriteLine("ingrese el peso de la carga en KG para evitar problemas al despegar: ");
+            peso_carga = Convert.ToDouble(Console.ReadLine());
+            if (peso_carga < 5000)
+            {
+                
+                Console.WriteLine("Despegue autorizado.Buen viaje, Mando");
+            }
+            else
+            {
+                Console.WriteLine("Alerta: Exceso de peso.Debes descargar "+ (peso_carga - 5000) +" kg para despegar");
+
+            }
+
+        }
+    }
+}
