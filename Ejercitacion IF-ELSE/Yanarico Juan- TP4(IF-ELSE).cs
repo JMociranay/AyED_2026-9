62af46f31432af7f79c9d02abfa4c07ecd0a3e8d
JMociranay <hildaj127@gmail.com>
2026-04-14 16:00:28 -0300

    Add files via upload

---
+++ Ejercitacion IF-ELSE/Yanarico Juan- TP4(IF-ELSE).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double compras_droides, descuento, total, precio_final;
+            Console.WriteLine("ingrese la cantidad de droides que comprara: ");
+            compras_droides = Convert.ToDouble(Console.ReadLine());
+            total = compras_droides * 2000;
+            descuento = total * 0.2;
+            precio_final = total - descuento;
+            if (compras_droides >= 3)
+            {
+                Console.WriteLine("el total del precio " + total);
+                Console.WriteLine("el descuento aplicado " + descuento);
+                Console.WriteLine("el precio final " + precio_final);
+            }
+            else
+            {
+                Console.WriteLine("el total del precio " + total);
+
+
+            }
+        }
+    }
+}
