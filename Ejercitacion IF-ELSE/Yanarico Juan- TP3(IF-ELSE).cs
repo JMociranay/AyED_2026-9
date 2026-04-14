9222ebb85949b4da6f4b488649bcd8e5500f80d2
JMociranay <hildaj127@gmail.com>
2026-04-14 15:42:59 -0300

    Add files via upload

---
+++ Ejercitacion IF-ELSE/Yanarico Juan- TP3(IF-ELSE).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double AñosLuz, combustible, faltante;
+            Console.WriteLine("ingrese la distacia hacia su destino en Años Luz: ");
+            AñosLuz = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese la cantidad de combustible en litros: ");
+            combustible = Convert.ToDouble(Console.ReadLine());
+            if (AñosLuz * 12 <= combustible)
+            {
+                Console.WriteLine("Cálculos precisos. Saltando al hiperespacio");
+            }
+            else
+            {
+                faltante = (AñosLuz * 12) - combustible;
+                Console.WriteLine("Peligro: Combustible insuficiente. Faltan  " + faltante + " litros");
+            }
+        }
+    }
+}
