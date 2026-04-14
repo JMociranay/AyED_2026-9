09f5b9d8ebc18d9f8a15bce1b9762735843d6e63
JMociranay <hildaj127@gmail.com>
2026-04-14 14:47:57 -0300

    Add files via upload

---
+++ Ejercitacion IF-ELSE/Yanarico Juan-TP2(IF-ELSE).cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double midictorianos;
+            Console.WriteLine("ingrese la cantidad de midiclorianos detectados en el sujeto: ");
+            midictorianos = Convert.ToDouble(Console.ReadLine());
+            if (midictorianos > 15000)
+            {
+                Console.WriteLine("Potencial Maestro Jedi detectado.Avisar al Consejo");
+            }
+            else if (5000 < midictorianos & midictorianos < 15000)
+            {
+                Console.WriteLine("Aceptable para entrenamiento Padawan");
+            }
+            else
+            {
+                Console.WriteLine("No posee sensibilidad a la Fuerza");
+
+            }
+
+
+        }
+    }
+}
+      
