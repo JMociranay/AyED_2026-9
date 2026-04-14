15865bac62510c1046570783e2b5b5d63d108793
JMociranay <hildaj127@gmail.com>
2026-04-14 16:36:15 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales/Yanarico Juan - TP1 (Condicionales).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double velocidad;
+            Console.Write("ingrese la velocidad del vehiculo: ");
+            velocidad = Convert.ToDouble(Console.ReadLine());
+            if (velocidad > 120)
+            {
+                Console.WriteLine("Multa en camino: Exceso de velocidad");
+            }
+            else
+            {
+                Console.WriteLine("Conducción segura");
+            }
+        }
+
+    }
+}
