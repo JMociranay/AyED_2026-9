06b53880c84733125dbcc441a4093398b56dda00
JMociranay <hildaj127@gmail.com>
2026-04-14 16:50:52 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales/Yanarico Juan -TP 3(Condicionales).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("ingrese un numero para indica si es par o impar: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("su numero es par");
+
+            }
+            else
+            {
+                Console.WriteLine("su numero es impar");
+            }
+        }
+    }
+}
