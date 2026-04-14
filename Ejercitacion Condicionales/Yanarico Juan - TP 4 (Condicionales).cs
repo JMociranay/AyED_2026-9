b9c3129e2455c1b425fdef116f9a7ed3685772c6
JMociranay <hildaj127@gmail.com>
2026-04-14 16:59:10 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales/Yanarico Juan - TP 4 (Condicionales).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota;
+            Console.Write("ingrese su nota de examen: ");
+            nota = Convert.ToDouble(Console.ReadLine());
+
+            if (nota >= 7)
+            {
+                Console.WriteLine("Promocionado");
+            }
+            else if (4 <= nota & nota <= 6)
+            {
+                Console.WriteLine("A finales");
+            }
+            else
+            {
+                Console.WriteLine("Recuperatorio");
+
+            }
+        }
+    }
+}
