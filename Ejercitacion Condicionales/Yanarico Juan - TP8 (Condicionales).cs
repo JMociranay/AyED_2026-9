fccc693ab1e2c69463c5e16e738b4a9296ebe005
JMociranay <hildaj127@gmail.com>
2026-04-21 14:24:45 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales/Yanarico Juan - TP8 (Condicionales).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            Console.Write("ingrese un numero: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+
+            if (numero % 5 == 0)
+            {
+                Console.WriteLine("su numero es divisible por 5");
+            }
+            else
+            {
+                Console.WriteLine("su numero no es divisible por 5");
+            }
+        }
+    }
+}
