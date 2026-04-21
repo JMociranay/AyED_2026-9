8fc75880025444ecd9a0a16a6f4d1bea53073a8d
JMociranay <hildaj127@gmail.com>
2026-04-21 14:42:08 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales/Yanarico Juan - TP10 (Condicionales).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double presupuesto, costo, faltante;
+            Console.Write(" ingrese el presupuesto: ");
+            presupuesto = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" ingrese el costo de los materiales: ");
+            costo = Convert.ToDouble(Console.ReadLine());
+
+            if (presupuesto < costo)
+            {
+                faltante = costo - presupuesto;
+                Console.WriteLine("el costo es mayor al presupuesto disponible, dinero prestado que se requiere: " + faltante);
+
+            }
+            else
+            {
+                Console.WriteLine("el presupuesto es suficiente");
+            }
+
+        }
+    }
+}
