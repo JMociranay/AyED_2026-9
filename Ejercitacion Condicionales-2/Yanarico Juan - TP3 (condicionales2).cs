2722c3e265250959cd057ca536d2fb304d8d0eda
JMociranay <hildaj127@gmail.com>
2026-04-21 15:43:40 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales-2/Yanarico Juan - TP3 (condicionales2).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String sexo;
+            double edad;
+            Console.Write("ingrese su sexo (M si es masculino) (F si es femenino): ");
+            sexo = Console.ReadLine();
+            Console.Write("ingrese su edad: ");
+            edad = Convert.ToDouble(Console.ReadLine());
+
+            if ((sexo == "F" && edad >= 65 ) || ( sexo == "M" && edad >= 60))
+            {
+                Console.WriteLine("puede iniciar el tramite jubilatorio");
+            }
+
+
+        }
+    }
+}
