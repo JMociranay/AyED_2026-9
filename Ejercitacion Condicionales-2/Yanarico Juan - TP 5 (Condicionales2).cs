98e99effd758d9dda8aa194caba64c52fd9c7a17
JMociranay <hildaj127@gmail.com>
2026-04-21 16:03:43 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales-2/Yanarico Juan - TP 5 (Condicionales2).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int segundos, minutos;
+            Console.Write("ingrse una cantidad de segundos: ");
+            segundos = Convert.ToInt32(Console.ReadLine());
+            if ( segundos >= 60)
+            {
+                minutos = segundos / 60;
+                segundos = segundos % 60;
+                Console.WriteLine("minutos :" + minutos + ", segundos: " + segundos);
+
+            }
+            else
+            {
+                Console.WriteLine("segundos: " + segundos);
+            }
+        }
+    }
+}
