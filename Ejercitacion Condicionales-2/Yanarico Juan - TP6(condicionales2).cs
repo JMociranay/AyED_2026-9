3f3e90bcb0cd36842724318cca6ea61e100c7426
JMociranay <hildaj127@gmail.com>
2026-04-21 16:12:05 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales-2/Yanarico Juan - TP6(condicionales2).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distancia, promedio;
+            Console.Write("ingrse su promedio: ");
+            promedio = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ingrese la distancia de su casa a la facultad: ");
+            distancia = Convert.ToDouble(Console.ReadLine());
+
+            if ((distancia >= 50) || (promedio >= 8))
+            {
+                Console.WriteLine("Beca Concedida");
+            }
+            else
+            {
+                Console.WriteLine("beca no concedida");
+            }
+
+        }
+    }
+}
