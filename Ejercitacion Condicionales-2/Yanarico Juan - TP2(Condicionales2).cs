f7c4989b3c6f04281e1705d5970d7e1c04ca4f30
JMociranay <hildaj127@gmail.com>
2026-04-21 15:27:08 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales-2/Yanarico Juan - TP2(Condicionales2).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, peso, IMC;
+            Console.Write("ingrese su altura en M: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ingrese su peso en KG: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            IMC = peso / (altura * altura);
+            if (IMC > 25)
+            {
+                Console.WriteLine("Sobrepeso: "+ IMC);
+
+
+            }
+            else
+            {
+                Console.WriteLine("Rango Normal: "+ IMC);
+            }
+        }
+    }
+}
