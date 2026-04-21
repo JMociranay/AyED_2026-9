58dd8ebb6665231f5b245268feeef6b661993120
JMociranay <hildaj127@gmail.com>
2026-04-21 14:20:02 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales/Yanarico Juan - TP7 (condicionales).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperatura;
+            Console.Write("ingrese la temperatura: ");
+            temperatura = Convert.ToDouble(Console.ReadLine());
+
+            if (temperatura < 0)
+            {
+                Console.WriteLine("alerta de frio extremo");
+            }
+            else if (temperatura > 0 & temperatura <= 15)
+            {
+                Console.WriteLine("mucho frio");
+
+            }
+            else if (temperatura > 15)
+            {
+                Console.WriteLine("crima agradable");
+            }
+        }
+    }
+}
