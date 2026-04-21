a4f4bec19f91e9e1ef67aedd56b7b4a7ba20a514
JMociranay <hildaj127@gmail.com>
2026-04-21 14:13:25 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales/Yanarico Juan - TP6(Condicionales).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, contraseña;
+            Console.Write("inbgrese un nombre de usuario: ");
+            nombre = Console.ReadLine();
+            Console.Write("ingrese una contraseña: ");
+            contraseña = Console.ReadLine();
+
+            if (nombre == "admin" & contraseña == "1234")
+            {
+                Console.WriteLine("Acceso Cocedido");
+
+            }
+            else
+            {
+                Console.WriteLine("error de credenciales");
+            }
+            
+        }
+    }
+}
