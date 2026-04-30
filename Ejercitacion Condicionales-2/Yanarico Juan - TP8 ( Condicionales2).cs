a79b534016c23b5b20b44d9c0d9029be479870da
JMociranay <hildaj127@gmail.com>
2026-04-30 14:15:04 -0300

    Add files via upload

---
+++ Ejercitacion Condicionales-2/Yanarico Juan - TP8 ( Condicionales2).cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.Write("ingrese el primer numero: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ingrese el segundo numero: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ingrese el tercer numero: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine("el numero " + a + " es mayor");
+                }
+                else
+                {
+                    Console.WriteLine("el numero " + c + " es mayor");
+                }
+            }
+            else if (b > c)
+            {
+                Console.WriteLine("el numero " + b + " es mayor");
+            }
+            else
+            {
+                Console.WriteLine("el numero " + c + " es mayor");
+            }
+        }
+    }
+}
