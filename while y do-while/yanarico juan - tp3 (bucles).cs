70bce10a7426aac65752335c8e4b252d0795b1df
JMociranay <hildaj127@gmail.com>
2026-05-19 16:20:59 -0300

    Add files via upload

---
+++ while y do-while/yanarico juan - tp3 (bucles).cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int sumador = 0;
+            do
+            {
+                Console.Write("ingrese un numero para sumar: ");
+                numero = int.Parse(Console.ReadLine());
+                sumador = sumador + numero;
+            }
+            while (numero != 0);
+            Console.WriteLine("la suma de los numeros " + sumador);
+        }
+    }
+}
