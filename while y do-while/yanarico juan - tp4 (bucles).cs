37d26c2a848de06d06214fa1f048484a3a9f853a
JMociranay <hildaj127@gmail.com>
2026-05-19 16:35:46 -0300

    Add files via upload

---
+++ while y do-while/yanarico juan - tp4 (bucles).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+            Console.Write("ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+            int contador = 1;
+            while (contador <= 12)
+            {
+                resultado = numero * contador;
+                Console.WriteLine(resultado);
+                contador++;
+
+            }
+            
+        }
+    }
+}
