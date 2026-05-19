914b501fa642fd79161e4328bb7cd925ba9023cf
JMociranay <hildaj127@gmail.com>
2026-05-19 15:20:08 -0300

    Add files via upload

---
+++ while y do-while/yanarico juan -tp1(bucles).cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("ingrese un numero para la cuentra regresiva: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0)
+            {
+                Console.WriteLine("numero " + numero);
+                numero--;
+            }
+        }
+    }
+}
