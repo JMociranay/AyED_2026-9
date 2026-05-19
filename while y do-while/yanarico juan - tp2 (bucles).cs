51c016c073165607a0bd95234880f283c500fddb
JMociranay <hildaj127@gmail.com>
2026-05-19 15:51:33 -0300

    Add files via upload

---
+++ while y do-while/yanarico juan - tp2 (bucles).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            do
+            {
+                Console.Write("ingrese un valor de nota del 1-10 (entero): ");
+                nota = int.Parse(Console.ReadLine());
+                if ((nota < 1) || (nota > 10))
+                {
+                    Console.WriteLine("nota no valida ,ingrese de nuevo: ");
+                }
+            }
+            while ((nota < 1) || (nota > 10));
+            Console.WriteLine("la nota correcta es " + nota);
+
+        }
+    }
+}
