50af2eddfb0a44e34bcf5ef6a4e54a49950f2979
JMociranay <hildaj127@gmail.com>
2026-05-19 16:49:37 -0300

    Add files via upload

---
+++ while y do-while/yanarico juan - tp5 (bucles).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clave;
+            string clave_correcta = "pikachu";
+
+            do
+            {
+                Console.Write("ingrese la clave de texto: ");
+                clave = Console.ReadLine();
+                if (clave != clave_correcta)
+                {
+                    Console.WriteLine("clave incorrecta ingrese denuevo");
+
+                }
+
+            }
+            while (clave != clave_correcta);
+            Console.WriteLine("acceso pernutido");
+
+        }
+    }
+}
