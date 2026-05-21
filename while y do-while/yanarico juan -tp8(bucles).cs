6d3f562ecf4a0a7d1e9d60ff073aa217b4cd8e01
JMociranay <hildaj127@gmail.com>
2026-05-21 15:47:04 -0300

    Add files via upload

---
+++ while y do-while/yanarico juan -tp8(bucles).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperatura;
+            Console.Write("ingrese una temperatura inicial: ");
+            temperatura = int.Parse(Console.ReadLine());
+
+            while (temperatura >= 24)
+            {
+                temperatura = temperatura - 2;
+                Console.WriteLine("enfriando...temperatura actual " + temperatura + "C°");
+
+            }
+            Console.WriteLine("´temperatura IDEAL alcanzada.");
+
+        }
+    }
+}
---
+++ while y do-while/yanarico juan -tp9(bucles).cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool salir = true;
+            while (salir == true)
+                {
+                Console.WriteLine("MENU SIMPLE");
+                Console.WriteLine("1. SALUDAR");
+                Console.WriteLine("2. DESPEDIRSE");
+                Console.WriteLine("3. SALIR");
+                Console.WriteLine(" seleccione una opcion: ");
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("elegiste saludarlo, -te devuelve el saludo-");
+                        break;
+                    case 2:
+                        Console.WriteLine("elegiste despedirte, -extrañado se despide-");
+                        break;
+                    case 3:
+                        Console.WriteLine("saliendo del programa");
+                        salir = false;
+                        break;
+                    default:
+                        Console.WriteLine("opcion no valida");
+                        break;
+                }
+
+            }
+           
+
+        }
+    }
+}
