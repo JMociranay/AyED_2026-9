16836396c28416804aed85eb851a1e97377e126a
JMociranay <hildaj127@gmail.com>
2026-05-21 14:45:16 -0300

    Add files via upload

---
+++ while y do-while/yanarico juan - tp6 (bucles).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int contador = 1;
+            Console.Write("ingrese un numero limite: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (contador <= numero)
+            {
+                if (contador % 2 == 0)
+                {
+                    Console.WriteLine(contador);
+                }
+                contador++;
+            }
+        }
+    }
+}
---
+++ while y do-while/yanarico juan - tp7 (bucles).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            double promedio, nota;
+            double sumador = 0;
+
+            while (contador <= 5)
+            {
+                Console.Write("ingrese la primera nota: ");
+                nota = Convert.ToDouble(Console.ReadLine());
+                sumador = sumador + nota;
+                contador++;
+            }
+            promedio = sumador / 5;
+            Console.WriteLine("promedio de sus notas " + promedio);
+
+
+
+
+        }
+    }
+}
