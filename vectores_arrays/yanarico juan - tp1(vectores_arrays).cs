aef4c3a69dd1cceac2eccd5d1dfdcf5a57da5bb4
JMociranay <hildaj127@gmail.com>
2026-08-11 16:37:51 -0300

    Add files via upload

---
+++ vectores_arrays/yanarico juan - tp1(vectores_arrays).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mayor = 0;
+            double menor = 0;
+            double total = 0;
+            double[] temperatura = new double[7];
+            for (int i = 0; i < temperatura.Length; i++)
+            {
+                Console.Write("ingrese la temperatura "  +(i + 1) + " :");
+                temperatura[i] = Convert.ToDouble(Console.ReadLine());
+                total += temperatura[i];
+                if (i == 0)
+                {
+                    mayor = temperatura[i];
+                    menor = temperatura[i];
+                }
+                else
+                {
+                    if (temperatura[i] > mayor)
+                    {
+                        mayor = temperatura[i];                
+                    }
+                    if (temperatura[i] < menor)
+                    {
+                        menor = temperatura[i];
+                    }
+                }
+            }
+            Console.WriteLine("Temperatura más alta: " + mayor);
+            Console.WriteLine("Temperatura más baja: " + menor);
+            Console.WriteLine("Promedio semanal: " + (total / temperatura.Length));
+        }
+    }
+}
