32695ac68f4e3a4de35f9078202578dca375b75e
JMociranay <hildaj127@gmail.com>
2026-08-13 16:04:17 -0300

    Add files via upload

---
+++ vectores_arrays/yanarico juan - tp3(vectores array).cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ausencias = 0;
+            int dias = 0;
+            string[] clases = new string[20];
+            for (int i = 0; i < clases.Length; i++)
+            {
+                dias++;
+                Console.Write("ingrese si tuvo asistencia el dia " + dias + " (presente = p)(ausente = a): ");
+                clases[i] = Console.ReadLine();
+                string asistencia = clases[i];
+                if ((asistencia == "A") || (asistencia == "a"))
+                {
+                    ausencias++;
+                }
+            }
+            if (ausencias > 6)
+            {
+                Console.WriteLine("alumno libre con "+ ausencias +" ausencias");
+            }
+            else
+            {
+                Console.WriteLine("alumno regular con "+ ausencias + " ausencias" );
+            }
+        }
+    }
+}
---
+++ vectores_arrays/yanarico juan - tp´4(vectores-array).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidad = 0;
+            Double[] precios = new double[8];
+            for (int i = 0; i < precios.Length; i++)
+            {
+                Console.WriteLine("ingrese el precio del producto "+ (i+1) +" : ");
+                precios[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("ingrese su monto de dinero: ");
+            double monto = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i < precios.Length; i++)
+            {
+                if (precios[i] <= monto)
+                {
+                    cantidad++;
+                }
+            }
+            Console.WriteLine("Puede comprar " + cantidad + " productos distintos.");
+        }
+    }
+}
