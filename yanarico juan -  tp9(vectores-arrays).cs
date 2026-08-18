6708eaaa865c9559ed39314e0c23519f207f0e36
JMociranay <hildaj127@gmail.com>
2026-08-18 16:22:46 -0300

    Add files via upload

---
+++ yanarico juan -  tp9(vectores-arrays).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 1;
+            int[] precios = new int[10];
+            for (int i = 0; i < precios.Length; i++)
+            {
+                Console.Write("ingrese el precio del producto " + (i + 1) + " : ");
+                precios[i] = int.Parse(Console.ReadLine());
+                total += precios[i];
+            }
+            total -= 1;
+            Console.WriteLine("ingrese el numero de producto que desea descontar del 1-10: ");
+            int opcion = int.Parse(Console.ReadLine());
+            opcion -= 1;
+            int Producto = precios[opcion];
+            Console.WriteLine("el precio del producto "+ (opcion +1) + " es "+ Producto);
+            Console.WriteLine("el total era de " + total + " ,ahora el nuevo total es de " + (total - Producto));
+        }
+    }
+}
